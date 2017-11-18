70ebe7343d2262050baa9de9487a74104958fa80
fredkingham <fredkingham@gmail.com>
2017-11-18 11:41:28 +0000

    adds in the ignore loader

---
+++ csv_schema/static/webpack.config.js
@@ -0,0 +1,34 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['./css/styles.scss'],
+  output: {
+    filename: 'dist/bundle.js'
+  },
+  watch: true,
+  module: {
+    rules: [
+      /*
+      your other rules for JavaScript transpiling go in here
+      */
+      {
+        test: /\.svg$/, use: "ignore-loader"
+      },
+      { // regular css files
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1'
+        })
+      },
+      { // sass / scss loader for webpack
+        test: /\.(sass|scss)$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({ // define where to save the file
+      filename: 'css/styles.css',
+    }),
+  ],
+};
