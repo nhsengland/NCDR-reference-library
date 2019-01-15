f5e0bc62890669f30f1266bcce4ff6a36e95673b
George Hickman <george@ghickman.co.uk>
2019-01-15 15:36:27 +0000

    Add MetricForm
    This got missed out, oops!

---
+++ metrics/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import Metric
+
+
+class MetricForm(forms.ModelForm):
+    class Meta:
+        model = Metric
+        fields = '__all__'
+        widgets = {
+            'is_table': forms.RadioSelect
+        }
