95536853639d67cdb2924a75914b3ef82f79be55
George Hickman <george@ghickman.co.uk>
2019-02-15 10:26:40 +0000

    Remove unused setting

--- ncdr/settings.py
+++ ncdr/settings.py
@@ -129,5 +129,3 @@ USE_TZ = True
 # Custom User Model
 # https://docs.djangoproject.com/en/2.1/topics/auth/customizing/#substituting-a-custom-user-model
 AUTH_USER_MODEL = "ncdr.User"
-
-DATA_UPLOAD_MAX_NUMBER_FIELDS = 100000
