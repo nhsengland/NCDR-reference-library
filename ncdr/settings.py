d3651fb2dd4b0e53d6d1add1de8d91b4b6e9805a
George Hickman <george@ghickman.co.uk>
2019-02-15 10:26:59 +0000

    Fix language and configure i18n, l10, & tz

--- ncdr/settings.py
+++ ncdr/settings.py
@@ -115,15 +115,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
+LANGUAGE_CODE = "en-gb"
 TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_L10N = True
-
+USE_I18N = False
+USE_L10N = False
 USE_TZ = True
 
 # Custom User Model
