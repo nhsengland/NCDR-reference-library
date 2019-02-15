a85eed7d8fba57927a40b0e90a7d1ccb33d2f44a
George Hickman <george@ghickman.co.uk>
2019-02-15 16:22:22 +0000

    Paginate data elements on Group detail page

--- ncdr/views/grouping.py
+++ ncdr/views/grouping.py
@@ -1,32 +1,44 @@
 from django.db.models import Prefetch
-from django.views.generic import DetailView, ListView
+from django.http import Http404
+from django.views.generic import ListView
 
-from ..models import Column, Grouping
+from ..models import Column, DataElement, Grouping
 from .base import ViewableItems
 
 
-class GroupingDetail(ViewableItems, DetailView):
-    model = Grouping
+class GroupingDetail(ViewableItems, ListView):
+    model = DataElement
+    paginate_by = 50
     template_name = "grouping_detail.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().get(request, *args, **kwargs)
+
+    def get_object(self):
+        try:
+            return Grouping.objects.viewable(self.request.user).get(
+                slug=self.kwargs["slug"]
+            )
+        except Grouping.DoesNotExist:
+            raise Http404
 
     def get_queryset(self):
         return (
             super()
             .get_queryset()
+            .filter(grouping=self.object)
+            .viewable(self.request.user)
             .prefetch_related(
                 Prefetch(
-                    "dataelement_set__column_set",
-                    queryset=Column.objects.viewable(self.request.user),
+                    "column_set", queryset=Column.objects.viewable(self.request.user)
                 )
             )
         )
 
     def get_context_data(self, **kwargs):
-        # get the list of tables in this database
         context = super().get_context_data(**kwargs)
-        context["groupings"] = Grouping.objects.viewable(self.request.user).order_by(
-            "name"
-        )
+        context["grouping"] = self.object
         return context
 
 
