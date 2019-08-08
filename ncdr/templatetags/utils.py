b7a35a7441adadaf1fda34c039f83d6d2bf57462
fredkingham <fredkingham@gmail.com>
2019-08-08 11:13:18 +0100

    Adds a paginator template tag for ease of use

--- ncdr/templatetags/utils.py
+++ ncdr/templatetags/utils.py
@@ -13,3 +13,24 @@ def chunked(iterable, chunk_size):
 @register.filter(name="url_name")
 def url_name(request):
     return resolve(request.path_info).url_name
+
+
+@register.inclusion_tag("template_tags/pagination.html", takes_context=True)
+def pagination(context):
+    """
+    Assumes we are in a list view with a paginator.
+
+    Adds a variable to the context which is the string
+    used by the paginator without the page attribute
+    so that we can set that ourselves.
+    """
+    query_dict = context["request"].GET.copy()
+    if "page" in query_dict:
+        query_dict.pop("page")
+    encoded_query = query_dict.urlencode()
+    if encoded_query:
+        query = f"&{encoded_query}"
+    else:
+        query = ""
+    context["paginator_query_string"] = query
+    return context
