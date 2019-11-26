aa514e2929ebe2dbf5c423924e8ee486c69b5014
fredkingham <fredkingham@gmail.com>
2019-11-26 13:25:07 +0000

    Changes the metrics  page as it were to point to the topics list

--- metrics/urls.py
+++ metrics/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import RedirectView
 from .views import About, AtoZList, MetricDetail, Search, TopicDetail, TopicList
 
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="metrics-list")),
+    path("", RedirectView.as_view(pattern_name="topics-list")),
     path("about/", About.as_view(), name="metrics-about"),
     path("a-z", AtoZList.as_view(), name="metrics-list"),
     path("topic", TopicList.as_view(), name="topics-list"),
