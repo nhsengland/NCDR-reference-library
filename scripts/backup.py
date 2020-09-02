5cd49fe0f0d4ff24431ff6c4a9f5fb2d370f2242
fredkingham <fredkingham@gmail.com>
2020-09-02 12:34:46 +0100

    Adds in a function to delete old backups

--- scripts/backup.py
+++ scripts/backup.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import boto3
 
@@ -26,13 +26,31 @@ def upload(bucket_name, local_path, key):
     s3.Bucket(bucket_name).upload_file(local_path, f"backups/{key}")
 
 
-def main(db_name, db_user, backups_dir, bucket_name):
-    today = datetime.now().strftime("%Y-%m-%d")
-    backup_name = f"{today}-{db_name}.dump"
+def get_backup_name(some_datetime, db_name):
+    dt_str = some_datetime.strftime("%Y-%m-%d")
+    return f"{dt_str}-{db_name}.dump"
 
+
+def main(db_name, db_user, backups_dir, bucket_name):
+    backup_name = get_backup_name(datetime.now(), db_name)
     full_backup_name = os.path.join(backups_dir, backup_name)
     dump_database(db_name, db_user, full_backup_name)
     upload(bucket_name, full_backup_name, backup_name)
+    delete_old_backups(backups_dir, db_name)
+
+
+def delete_old_backups(backups_dir, db_name):
+    now = datetime.now()
+    to_keep = set()
+    for i in range(30):
+        some_dt = now - timedelta(i)
+        to_keep.add(get_backup_name(some_dt, db_name))
+
+    for file_name in os.listdir(backups_dir):
+        if file_name not in to_keep:
+            full_file = os.path.join(backups_dir, file_name)
+            print("deleting {}".format(full_file))
+            os.remove(full_file)
 
 
 if __name__ == "__main__":
