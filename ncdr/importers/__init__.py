1c1ca86b6e0d8046271ef40691727c2b5041aa65
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-03-09 19:33:23 +0000

    Change the check function to actually check the table in the upstream db

--- ncdr/importers/__init__.py
+++ ncdr/importers/__init__.py
@@ -1,7 +1,8 @@
 from django.db import transaction
 import structlog
 import traceback
-from ncdr.importers import column, grouping, table
+from ncdr.importers import column, grouping, table, db_api
+from ncdr.models import Version
 
 logger = structlog.get_logger("ncdr")
 
@@ -11,13 +12,17 @@ def check():
     They will provide a time stamp for a version to let us know if
     when a new version is available. This is not yet done.
     """
-    return True
+    query = "Select * from tbl_Export_Standard_RefreshDateTime"
+    result = db_api.query(query)[0]["Refresh_DateTime"]
+    if result > Version.objects.order_by("-created_at")[0].created_at:
+        return True
 
 
 @transaction.atomic
-def check_and_import(version):
+def check_and_import():
     try:
         if check():
+            version = Version.objects.create(is_published=False)
             table.import_from_db(version)
             column.import_from_db(version)
             grouping.import_from_db(version)
--- ncdr/management/commands/check_and_create_new_version.py
+++ ncdr/management/commands/check_and_create_new_version.py
@@ -1,10 +1,8 @@
 from django.core.management.base import BaseCommand
 
 from ncdr.importers import check_and_import
-from ncdr.models import Version
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        version = Version.objects.create(is_published=False)
-        check_and_import(version)
+        check_and_import()
