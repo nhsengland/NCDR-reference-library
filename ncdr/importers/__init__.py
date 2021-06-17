48e190ce1b10993a8f3f576462854479d18b3187
fredkingham <fred.kingham@openhealthcare.org.uk>
2021-06-17 20:42:38 +0100

    Adds additional logging around the length of time it takes to run the managment command

--- ncdr/importers/__init__.py
+++ ncdr/importers/__init__.py
@@ -2,6 +2,7 @@ from django.db import transaction
 from django.utils import timezone
 import structlog
 import traceback
+import time
 from ncdr.importers import column, grouping, table, db_api
 from ncdr.models import Version
 
@@ -21,11 +22,14 @@ def check():
 
 @transaction.atomic
 def check_and_import():
+    start = time.time()
     try:
         if check():
             version = Version.objects.create(is_published=False)
             table.import_from_db(version)
             column.import_from_db(version)
             grouping.import_from_db(version)
+            end = time.time()
+            logger.info(f"Version loaded in {(end-start)/60} minutes")
     except Exception:
         logger.error(f"Unable to load in NCDR \n{traceback.format_exc()}")
